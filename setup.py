f2fb1d9ee42b50aaa9fbb139f38f2eac373471db
Kanishkan Kukarajah <kanishkan77@gmail.com>
2021-03-23 01:14:48 -0400

    version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
         name="Kanishkan-Test",
-        version="0.1.1",
+        version="0.1.2",
         packages=find_packages(),
         description="Test Package to try functionality",
         long_description=long_description,
